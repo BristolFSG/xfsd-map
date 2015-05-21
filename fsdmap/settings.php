7d142a1d41d119bb4325d0de915bda1754d67386
Andrew Parish <andrew@parishfamily.co.uk>
2015-05-21 23:18:06 +0100

    Simple change of settings to stand the program up on the BFSG server

--- fsdmap/settings.php
+++ fsdmap/settings.php
@@ -1,5 +1,5 @@
 <?php
-define('WHAZZUP_URL','http://x-flightserver.net/whazzup.txt');
-define('UPDATE_FREQ',9000);//map refresh time in ms
-define('API_KEY','ABQIAAAAqgd_2ErPbsDiFOLWspGXURTUfd3hJhdysthROlY4QlL2dcxZGRSRwBZ8ML_8TFPJOqPHlYFzZV6h4Q');
+define('WHAZZUP_URL','http://bfsg.game-host.org/fsd/whazzup.txt');
+define('UPDATE_FREQ',60000);//map refresh time in ms
+define('API_KEY','ABQIAAAAw8R82MiXjPNpE6Gs_K2ZThQtBfqMC5bwFu5bNNT3ZxRTSSgd7RQd3x4n0DRzcO3kDjorYaxbuv_Eug');
 ?>
